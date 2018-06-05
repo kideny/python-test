609931aeb75685e96088b8a671d7edf83c31d29d
kideny <kideny@gmail.com>
2018-06-05 15:04:32 +0800

    add tests

--- pythontest/tests/language/basic/data_type/TestStringMethods.py
+++ pythontest/tests/language/basic/data_type/TestStringMethods.py
@@ -2,13 +2,21 @@ import unittest
 
 
 class TestStringMethods (unittest.TestCase):
+    string1 = 'foo'
+    string2 = 'Foo'
+    string3 = 'FOO'
+    string4 = 'hello world'
 
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual(self.string1.upper(), 'FOO')
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue(self.string3.isupper())
+        self.assertFalse(self.string2.isupper())
+
+    def test_islower(self):
+        self.assertTrue(self.string1.islower())
+        self.assertFalse(self.string2.islower())
 
     def test_split(self):
         s = 'hello world'
